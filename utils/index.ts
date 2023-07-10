bacd6e4459ed56cf2381bc0f3d849d967106377f
Karo <karolp2003@gmail.com>
2023-07-10 22:11:01 +0200

    Fixed typo

--- utils/index.ts
+++ utils/index.ts
@@ -35,7 +35,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
-  const API_KEY = "hrjavacript-mastery";
+  const API_KEY = "hrjavascript-mastery";
   const { make, year, model } = car;
   url.searchParams.append("customer", API_KEY ?? "");
   url.searchParams.append("make", make);
